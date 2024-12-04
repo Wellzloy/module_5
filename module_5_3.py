6257d8678ae5309d77ec029d92191193c95a8f12
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-12-04 22:53:48 +0400

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -19,15 +19,34 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
-
     def __eq__(self, other):
         return self.number_of_floors == other.number_of_floors
 
-
     def __add__(self, value):
-        self.number_of_floors = self.number_of_floors + value
-        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+        return self
 
+    def __iadd__(self, value):
+        self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -40,5 +59,16 @@ print(h1 == h2)
 
 h1 = h1 + 10
 print(h1)
-
 print(h1 == h2)
+
+h1 += 10
+print(h1)
+
+h2 = 10 + h2
+print(h2)
+
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
