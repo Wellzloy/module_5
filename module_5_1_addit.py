a52dc4c2835e95daae4612b30dd3163c8e4e2417
Wellzloy <93443716+Wellzloy@users.noreply.github.com>
2024-12-05 21:57:08 +0400

    Add files via upload

--- module_5_1_addit.py
+++ module_5_1_addit.py
@@ -3,29 +3,28 @@ class Person:
         self.__name = name  # устанавливаем имя
         self.__age = age  # устанавливаем возраст
 
-    # сеттер для установки возраста
-    def set_age(self, age):
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
         if 0 < age < 110:
             self.__age = age
         else:
             print("Недопустимый возраст")
 
-    # геттер для получения возраста
-    def get_age(self):
-        return self.__age
+    @property
+    def name(self):
+        return self.__name
 
-    def set_name(self, name):
+    @name.setter
+    def name(self, name):
         names = ['Tom', 'Ivan', 'Anton']
-        print(name)
-        print(names)
-        if name is names:
+        if name in names:
             self.__name = name
         else:
             print("Недопустимое имя")
-
-    # геттер для получения имени
-    def get_name(self):
-        return self.__name
 
     def print_person(self):
         print(f"Имя: {self.__name}\tВозраст: {self.__age}")
@@ -33,8 +32,8 @@ class Person:
 
 tom = Person("Tom", 39)
 tom.print_person()  # Имя: Tom  Возраст: 39
-# tom.set_age(-3486)  # Недопустимый возраст
-tom.set_age(25)
-tom.set_name('Ivan')
-tom.set_age(12)
+tom.age = 56  # Недопустимый возраст
+tom.age = 25
+tom.name = 'Ivan'  # Недопустимое имя
+tom.age = 12
 tom.print_person()  # Имя: Tom  Возраст: 25
